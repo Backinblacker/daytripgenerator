30c037c1297da5a922accc8467abe62e7b0b3e58
Kyle Blacker <coachblacker@gmail.com>
2023-02-10 12:44:16 -0700

    Created VS code file day_trip_generator for generator to the folder

---
+++ day_trip_generator.py
@@ -0,0 +1,11 @@
+destinations_list = ['Denver', '']
+restaurants_list =[]
+mode_of_transport = []
+entertainment_list = []
+# def run_day_trip_generator():
+# def print_full_trip(list_of_options):
+# def generate_random_item(list_of_items):
+# def degermins_satisfaction(current_trip, trip_options):
+# def re_select_option(current_trip, options):
+    
+# run_day_trip_generator()
