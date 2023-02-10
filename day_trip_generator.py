61a6220bf43ed481798690252c63701861a8ffd2
Kyle Blacker <coachblacker@gmail.com>
2023-02-10 15:34:01 -0700

    Created Framework for user input for what option they do not like? Stuck here with figuring out how to create a single function that can get a random selection from any list that gets passed in. That way you can reuse the same function multiple times and pass in a different list each time it is called per instruction

--- day_trip_generator.py
+++ day_trip_generator.py
@@ -12,23 +12,25 @@ entertainment_list = ['Musical', 'Haunted Town Tour', 'Gone Fishing', 'Squirrel 
 def generate_random_dest():
     random_dest = random.choice(destinations_list)
     print('Destination: ' + random_dest)
-
+    return random_dest
 generate_random_dest()
 
 def generate_random_rest():
     random_rest = random.choice(restaurants_list)
     print('Restaurant: ' + random_rest)
+    return random_rest
 generate_random_rest()
 
 def generate_random_mode():
     random_mode = random.choice(mode_of_transport)
     print('Mode of Transportation: ' + random_mode)
+    return random_mode
 generate_random_mode()
 
 def generate_random_entertain():
     random_entertain = random.choice(entertainment_list)
     print('Entertainment: ' + random_entertain)
-    return
+    return random_entertain
 generate_random_entertain()
 
 
@@ -43,6 +45,19 @@ generate_random_entertain()
 #             print(f"Congrats! Let's get your {current_trip} booked.")
 #             trip_satifaction = False
 # determine_satisfaction()
+
+# Which option would they change:
+# def change_option():
+#     user_input = ("Which option would you like to change? Destination, Restaurant, Transportation, Entertainment")
+#     if user_input == "Destination" or "destination":
+#     if user_input == "Restaurant" or "restaurant":
+#     if user_input == "Transportation" or "transportation":
+#     if user_input == "Entertainment" or "entertainment":
+# Is it the Destination?
+# def bad_destination():
+#     not_this_one = True
+#     while not_this_one == True:
+#         user_input = 
         
 # def re_select_option(current_trip, options):
     
