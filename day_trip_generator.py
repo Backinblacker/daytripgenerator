c80bc31c0e1e87a4ceaab79cf94660dc10886b8b
Kyle Blacker <coachblacker@gmail.com>
2023-02-10 14:47:39 -0700

    Generate Destination, Restaurant, Mode of Transport, and entertainment functions created

--- day_trip_generator.py
+++ day_trip_generator.py
@@ -8,22 +8,42 @@ entertainment_list = ['Musical', 'Haunted Town Tour', 'Gone Fishing', 'Squirrel 
 
 # def run_day_trip_generator():
 # def print_full_trip(list_of_options):
-#def generate_random_item(list_of_items):
-def generate_random_item():
+
+def generate_random_dest():
     random_dest = random.choice(destinations_list)
-    print(random_dest)
+    print('Destination: ' + random_dest)
 
+generate_random_dest()
+
+def generate_random_rest():
     random_rest = random.choice(restaurants_list)
-    print(random_rest)
+    print('Restaurant: ' + random_rest)
+generate_random_rest()
 
+def generate_random_mode():
     random_mode = random.choice(mode_of_transport)
-    print(random_mode)
+    print('Mode of Transportation: ' + random_mode)
+generate_random_mode()
 
+def generate_random_entertain():
     random_entertain = random.choice(entertainment_list)
-    print(random_entertain)
+    print('Entertainment: ' + random_entertain)
     return
-generate_random_item()
-# def degermins_satisfaction(current_trip, trip_options):
+generate_random_entertain()
+
+
+# def determine_satisfaction():
+#     trip_satifaction = True
+#     current_trip = generate_random_item()
+#     while trip_satifaction == True:
+#         user_input = input(f"Is {current_trip} satifactory, Y or N?")
+#         if user_input == "N":
+#             print(f"Let's try this again.")
+#         elif user_input == "Y":
+#             print(f"Congrats! Let's get your {current_trip} booked.")
+#             trip_satifaction = False
+# determine_satisfaction()
+        
 # def re_select_option(current_trip, options):
     
 # run_day_trip_generator()
