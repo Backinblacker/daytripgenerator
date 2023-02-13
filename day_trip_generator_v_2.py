6e39ea468a6da0a7676cd345445708095445e6a0
Kyle Blacker <coachblacker@gmail.com>
2023-02-13 16:20:52 -0700

    Took generator one and attempted to refactor the code to make the random generator inside of a function. This reduced the code and should have made it easier. See v.1 versus v.2.

--- day_trip_generator_v_2.py
+++ day_trip_generator_v_2.py
@@ -2,7 +2,7 @@ import random
 destinations_list = ['Colorado', 'Florida', 'Arizona', 'New York City', 'Seattle']
 restaurants_list =['Steak House', 'Sushi Den', 'Salad Central', 'What did I just eat?', 'Chicken Shack']
 mode_of_transport = ['Car', 'Bus', 'Plane', 'Train', 'Teleporter']
-entertainment_list = ['to a Musical', 'Haunted Town Tour', 'Fishing Tour', 'Squirrel Wrangling', 'Scavenger Hunt Tour']
+entertainment_list = ['Musical', 'Haunted Town Tour', 'Fishing Tour', 'Squirrel Wrangling', 'Scavenger Hunt Tour']
 
 # Random generate answer
 
@@ -11,7 +11,6 @@ def random_trip (list_of_options):
     return (output)
 
 def starting_choices (trip_elements):
-    trip_elements = ("", "", "", "")
     trip_elements[0] = random_trip (destinations_list)
     print("Destination:  ", trip_elements[0])
     trip_elements[1] = random_trip (restaurants_list)
@@ -35,7 +34,7 @@ def determine_satisfaction():
         user_input = input(f"Is your trip satifactory, Y or N? ")
         if user_input == "N" or user_input == "n":
             day_trip = change_option(trip_elements)
-            print(day_trip)
+            #print(day_trip)
         elif user_input == "Y" or user_input == "y":
             print(f"Congrats! Now let's get your trip booked.")
             trip_satifaction = False
@@ -43,22 +42,25 @@ def determine_satisfaction():
 # Which option would they change?:
 def change_option(trip_elements):
     user_input = input("Which option would you like to change? Destination, Restaurant, Transportation, Entertainment: ")
-    trip_elements = ["", "", "", ""]
     if user_input == "Destination" or user_input == "destination":
-        destinations_list.remove(trip_elements)
-        trip_elements[0] = random_trip (destinations_list)
+        destinations_list.remove(trip_elements[0])
+        trip_elements[0] = random_trip(destinations_list)
     elif user_input == "Restaurant" or user_input == "restaurant":
-        restaurants_list.remove(trip_elements)
+        restaurants_list.remove(trip_elements[1])
         trip_elements[1] = random_trip(restaurants_list)
     elif user_input == "Transportation" or user_input == "transportation":
-        mode_of_transport.remove(trip_elements)
+        mode_of_transport.remove(trip_elements[1])
         trip_elements[2] = random_trip(mode_of_transport)
     elif user_input == "Entertainment" or user_input == "entertainment":
-        entertainment_list.remove(entertain)
+        entertainment_list.remove(entertainment_list[3])
         trip_elements[3] = random_trip(entertainment_list)
     else:
         print('This is not a valid input.')
-        
+    
+    print("Destination:  ", trip_elements[0])
+    print("Restaurant:  ", trip_elements[1])
+    print("Mode of Transport:  ", trip_elements[2])
+    print("Entertainment:  ", trip_elements[3])
     return trip_elements
 
 determine_satisfaction()
