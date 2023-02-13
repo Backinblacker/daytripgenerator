935abd5192828aefc748219bd471996bc976f30e
Kyle Blacker <coachblacker@gmail.com>
2023-02-13 09:49:09 -0700

    removed a call of change on line 29 and added user_imput to the 'or' statements in change_option

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 DCC Project 1 Day Trip Generator
 
 Day Trip generator creation
+
+Day Trip Generator assignment:
+
+Main Stories
+
+
+- As a developer, I want to make at least three commits with descriptive messages 
+- As a developer, I want to store my destinations, restaurants, mode of transportation, and entertainment in their own separate Lists. 
+- As a user, I want a destination to be randomly selected for my day trip. 
+- As a user, I want a restaurant to be randomly selected for my day trip
+- As a user, I want a mode of transportation to be randomly selected for my day trip. 
+- As a user, I want a form of entertainment to be randomly selected for my day trip.
+- As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don’t like one or more of those things.
+- As a user, I want to be able to confirm that my day trip is “complete” once I like all of the random selections.
+- As a user, I want to display my completed trip in the console
+- Single Responsibility: As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing! 
--- day_trip_generator.py
+++ day_trip_generator.py
@@ -6,8 +6,6 @@ entertainment_list = ['Musical', 'Haunted Town Tour', 'Fishing', 'Squirrel Wrang
 
 # Random generate answer
 
-# def run_day_trip_generator():
-
 def random_trip (list_of_options):
     output = random.choice(list_of_options)
     return (output)
@@ -18,53 +16,42 @@ mode = random_trip (mode_of_transport)
 entertain = random_trip (entertainment_list)
 
 # day trip message
-day_trip = (f'Here is your trip. You have been selected take (a/the) {mode} to {destination}, eat at {restaurant}, and go {entertain}!')
+day_trip = (f'Here is your trip. You have been selected take a {mode} to {destination}, eat at {restaurant}, and go {entertain}!')
 print(day_trip)
 
+# does the trip that spit out meet the user's expectations?
 def determine_satisfaction(day_trip, mode, destination, restaurant, entertain, destinations_list, restaurants_list, mode_of_transport, entertainment_list):
     trip_satifaction = True
     while trip_satifaction == True:
         user_input = input(f"Is your trip satifactory, Y or N?")
         if user_input == "N":
-            change_option(mode, destination, restaurant, entertain, destinations_list, restaurants_list, mode_of_transport, entertainment_list)
             mode, destination, restaurant, entertain = change_option(mode, destination, restaurant, entertain, destinations_list, restaurants_list, mode_of_transport, entertainment_list)
-            day_trip = (f'Here is your trip. You have been selected take (a/the) {mode} to {destination}, eat at {restaurant}, and go {entertain}!')
+            day_trip = (f'Here is your trip. You have been selected take a {mode} to {destination}, eat at {restaurant}, and go {entertain}!')
             print(day_trip)
         elif user_input == "Y":
             print(f"Congrats! Let's get your trip booked.")
             trip_satifaction = False
 
-# Which option would they change:
+# Which option would they change?:
 def change_option(mode, destination, restaurant, entertain, destinations_list, restaurants_list, mode_of_transport, entertainment_list):
-    user_input = input("Which option would you like to change? Destination, Restaurant, Transportation, Entertainment")
-    if user_input == "Destination" or "destination":
+    user_input = input("Which option would you like to change? Destination, Restaurant, Transportation, Entertainment: ")
+    print(destination)
+    print(destinations_list)
+    if user_input == "Destination" or user_input == "destination":
         destinations_list.remove(destination)
         destination = random_trip(destinations_list)
-    if user_input == "Restaurant" or "restaurant":
+    elif user_input == "Restaurant" or user_input == "restaurant":
         restaurants_list.remove(restaurant)
         restaurant = random_trip(restaurants_list)
-    if user_input == "Transportation" or "transportation":
+    elif user_input == "Transportation" or user_input == "transportation":
         mode_of_transport.remove(mode)
         mode = random_trip(mode_of_transport)
-    if user_input == "Entertainment" or "entertainment":
+    elif user_input == "Entertainment" or user_input == "entertainment":
         entertainment_list.remove(entertain)
         entertain = random_trip(entertainment_list)
     else:
         print('This is not a valid input.')
         
     return mode,destination, restaurant, entertain
-# mode, destination, restaurant, entertain = change_option(mode, destination, restaurant, entertain, destinations_list, restaurants_list, mode_of_transport, entertainment_list)
-# day_trip = (f'Here is your trip. You have been selected take (a/the) {mode} to {destination}, eat at {restaurant}, and go {entertain}!')
-# print(day_trip)
+
 determine_satisfaction(day_trip, mode, destination, restaurant, entertain, destinations_list, restaurants_list, mode_of_transport, entertainment_list)
-
-
-# Is it the Destination?
-# def bad_destination():
-#     not_this_one = True
-#     while not_this_one == True:
-#         user_input = 
-        
-# def re_select_option(current_trip, options):
-    
-# run_day_trip_generator()
